7e872b559f403243bfbbdaeb9ed7393cbe1266a9
Filipp223 <filippfrolov@icloud.com>
2024-04-12 18:43:31 +0200

    added Nav.jsx

---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav>
+            <a className="link" href="#">Profile</a>
+            <a className="link" href="#">Messages</a>
+            <a className="link" href="#">Users</a>
+        </nav>
+    );
+}
+
+export default Nav;
