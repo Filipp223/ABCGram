ea32f8a5e2065edcfcaa55fa9184843d812a274f
Filipp223 <filippfrolov@icloud.com>
2024-05-03 18:53:37 +0200

    Modified Feed by seperating data from html code

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,13 +1,36 @@
 import styles from "./Feed.module.css";
 import FeedPost from "../FeedPost/FeedPost.jsx";
+const feedArray = [
+
+    {
+        title: "Lorem Ipsum",
+        location: "Rome, Italy",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores iste aliquam, unde voluptas excepturi necessitatibus quas maxime quos saepe vitae fugiat deleniti in pariatur nostrum nam! Voluptatem excepturi asperiores voluptate.",
+    },
+
+    {
+        title: "Ipsum Lorem",
+        location: "Buharest, Romania",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est perspiciatis fuga quas, quasi voluptatum! Aut voluptatem a incidunt ad cupiditate exercitationem aliquid eaque natus quaerat non amet, quam aperiam!",
+    },
+
+    {
+        title: "Morel Muspi",
+        location: "Cape Town, South Africa",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, nesciunt perspiciatis quae ipsum cumque totam vel expedita error fugiat dolore laborum voluptatum deleniti eligendi quod, dignissimos, aliquam fugit! Cum, officia.",
+    }
+];
 
 function Feed() {
     return (
         <div className={styles.feed}>
             <h2 className={styles.title}>Browse</h2>
-            <FeedPost title="Lorem ipsum" location="Rome, Italy" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores iste aliquam, unde voluptas excepturi necessitatibus quas maxime quos saepe vitae fugiat deleniti in pariatur nostrum nam! Voluptatem excepturi asperiores voluptate."/>
-            <FeedPost title="Ipsum Lorem" location="Buharest, Romania" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est perspiciatis fuga quas, quasi voluptatum! Aut voluptatem a incidunt ad cupiditate exercitationem aliquid eaque natus quaerat non amet, quam aperiam!"/>
-            <FeedPost title="Morel Muspi" location="Cape Town, South Africa" text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, nesciunt perspiciatis quae ipsum cumque totam vel expedita error fugiat dolore laborum voluptatum deleniti eligendi quod, dignissimos, aliquam fugit! Cum, officia."/>
+            {feedArray.map((feed, i) => {
+                return (<FeedPost title={feed.title} location={feed.location} text={feed.text} key={i}/>)
+            })}
+            {/* <FeedPost title={feedArray[0].title} location={feedArray[0].location} text={feedArray[0].text}/>
+            <FeedPost title={feedArray[1].title} location={feedArray[1].location} text={feedArray[1].text}/>
+            <FeedPost title={feedArray[2].title} location={feedArray[2].location} text={feedArray[2].text}/> */}
         </div>
     );
 }
--- src/components/FeedPost/FeedPost.jsx
+++ src/components/FeedPost/FeedPost.jsx
@@ -1,11 +1,20 @@
 import styles from "./FeedPost.module.css";
 
-function FeedPost(props) {
+function FeedPost({title, location, text}) {
+    /*let array = [1, 2, 3];
+    const [num1, num2, num3] = array;
+    console.log(num1, num2, num3);
+    const obj = {
+        name: "John",
+        age: 24
+    };
+    const {name, age} = obj;
+    console.log(name, age);*/
     return (
         <div className={styles.post}>
-            <h3 className={styles.subtitle}>{props.title}</h3>
-            <p className={styles.location}>{props.location}</p>
-            <p className={styles.text}>{props.text}</p>
+            <h3 className={styles.subtitle}>{title}</h3>
+            <p className={styles.location}>{location}</p>
+            <p className={styles.text}>{text}</p>
         </div>
     );
 }
