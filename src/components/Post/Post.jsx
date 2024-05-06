f03cc7ad279b0c182019107d8cc844064b167186
Filipp223 <filippfrolov@icloud.com>
2024-05-06 18:07:16 +0200

    changed props structure, separated data from structure

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,13 @@
-import profileImg from "../../img/Gyllenhaal.webp";
 import styles from "./Post.module.css";
 
 console.log(styles);
 
-function Post(props) {
+function Post({image, userName, postText}) {
     return (
         <li className={styles.post}>
-            <img className="profileImage" src={props.image} alt={props.userName} width="100"/>
-            <h3 className={styles.author}>{props.userName}</h3>
-            <p className="text">{props.postText}</p>
+            <img className="profileImage" src={image} alt={userName} width="100"/>
+            <h3 className={styles.author}>{userName}</h3>
+            <p className="text">{postText}</p>
         </li>
     );
 }
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -4,6 +4,26 @@ import profileImg1 from "../../img/Gyllenhaal.webp";
 import profileImg2 from "../../img/aaronpaul.jpg";
 import profileImg3 from "../../img/jeffbezos.jpg";
 
+const postArray = [
+    {
+        image: profileImg1,
+        userName: "Jake Gyllenhaal",
+        postText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam praesentium distinctio? Impedit aspernatur placeat hic iure voluptatum dolor officiis est adipisci magni mollitia incidunt, ab nostrum odio magnam omnis!",
+    },
+
+    {
+        image: profileImg2,
+        userName: "Aaron Paul",
+        postText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, earum voluptate! Ullam nulla fuga laudantium, rerum optio porro delectus amet, minima sed, quia praesentium atque deleniti? Recusandae atque obcaecati sequi.",
+    },
+
+    {
+        image: profileImg3,
+        userName: "Jeff Bezos",
+        postText: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores maxime excepturi debitis. Illo id magnam sint repellendus ad exercitationem in ipsum. Perspiciatis laborum hic voluptatibus reprehenderit ex fuga quam labore!",
+    }
+]
+
 function Posts() {
     return (
         <div className={styles.posts}>
@@ -11,9 +31,9 @@ function Posts() {
             <textarea className="textarea" placeholder="What is happening!"/>
             <button className="button">Post</button>
             <ul className="list">
-                <Post image={profileImg1} userName="Jake Gyllenhaal" postText="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam praesentium distinctio? Impedit aspernatur placeat hic iure voluptatum dolor officiis est adipisci magni mollitia incidunt, ab nostrum odio magnam omnis!"/>
-                <Post image={profileImg2} userName="Aaron Paul" postText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, earum voluptate! Ullam nulla fuga laudantium, rerum optio porro delectus amet, minima sed, quia praesentium atque deleniti? Recusandae atque obcaecati sequi."/>
-                <Post image={profileImg3} userName="Jeff Bezos" postText="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores maxime excepturi debitis. Illo id magnam sint repellendus ad exercitationem in ipsum. Perspiciatis laborum hic voluptatibus reprehenderit ex fuga quam labore!"/>
+                {postArray.map((post, i) => {
+                    return (<Post image={post.image} userName={post.userName} postText={post.postText} key={i}/>)
+                })}
             </ul>
         </div>
     )
